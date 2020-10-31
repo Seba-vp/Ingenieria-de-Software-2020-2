a8409e3d1b094ccd6c628fadff074bf56cd45e35
Seba <svillablanca1@uc.cl>
2020-10-30 22:52:58 -0300

    funciones varias

--- carreteapp/app/controllers/pages_controller.rb
+++ carreteapp/app/controllers/pages_controller.rb
@@ -1,13 +1,5 @@
 class PagesController < ActionController::Base
     def home
-        puts "Hello world "
-        @greeting = "La acción del controlador dice: Hola Mundo!"
-    end
-    
-    def saludar
-            "Hello world 2!"
-    end
-    def multiplicar(a)
-        a*2
+      @posts = Post.all
     end
 end
