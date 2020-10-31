c2f65ee288eb01b1db5c65bf48d1067670810ea0
Seba <svillablanca1@uc.cl>
2020-10-30 21:14:33 -0300

    funciones varias

--- carreteapp/app/models/comuna.rb
+++ carreteapp/app/models/comuna.rb
@@ -1,6 +1,6 @@
 class Comuna < ApplicationRecord
     validates :name, presence: true
-    has_many: users, dependent: :destroy
-    has_many: parties
-    has_many: services
+    has_many :users, dependent: :destroy
+    has_many :parties
+    has_many :services
 end
--- carreteapp/db/migrate/20201030053208_addid_creator_from_party.rb
+++
@@ -1,4 +0,0 @@
-class AddidCreatorFromParty < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
